a0d4a30dcad512d867f3f9c98360237c1b85425f
AlexisFVRX <alexis.fvrx@gmail.com>
2022-11-15 15:57:36 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const OpenApiValidator = require('express-openapi-validator');
+
+app.use(cors());
+app.use(express.json());
+
+const carsRouter = require('./routers/carsRouter');
+app.use('/books', carsRouter);
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+        .json({success: false, message: error.message, status: error.status});
+});
+
+module.exports = app;
