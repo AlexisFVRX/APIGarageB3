bec137246ff9122bd799d30ababb0e3ff4b6ae94
ValGLORIES <valmond.glories@ynov.com>
2022-11-16 08:47:35 +0100

    initial commit

--- routers/carsRouter.js
+++ routers/carsRouter.js
@@ -1,10 +1,19 @@
 const express = require('express'),
+
       router = express.Router(),
+
       carsController = require('../controller/cars.js');
 
+
+
 router.get('/', carsController.getCars);
+
 router.get('/:id', carsController.getCarById);
+
 router.post('/', carsController.addCar);
+
 router.delete('/:id', carsController.deleteCarById);
+
+
 
 module.exports = router;
