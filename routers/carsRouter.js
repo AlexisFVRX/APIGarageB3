a204b03652cf00769137969365b38a72af6511e2
AlexisFVRX <alexis.fvrx@gmail.com>
2022-11-16 08:41:51 +0100

    initial commit

--- routers/carsRouter.js
+++ routers/carsRouter.js
@@ -2,9 +2,9 @@ const express = require('express'),
       router = express.Router(),
       carsController = require('../controller/cars.js');
 
-router.get('/cars', carsController.getCars);
+router.get('/', carsController.getCars);
 router.get('/:id', carsController.getCarById);
-router.post('/addCar', carsController.addCar);
+router.post('/', carsController.addCar);
 router.delete('/:id', carsController.deleteCarById);
 
 module.exports = router;
